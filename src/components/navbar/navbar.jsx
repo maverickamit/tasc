7682937afb7bbfbd825a142b4b4322bb09850115
Amit <29959138+maverickamit@users.noreply.github.com>
2020-08-13 18:56:43 +0530

    created navbar component

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./navbar.css";
+
+const NavBar = () => (
+  <header className="navbar">
+    <div className="navbar__title navbar__item">Tasc</div>
+    <div className="navbar__item">Login</div>
+    <div className="navbar__item">Sign Up</div>
+    <div className="navbar__item">Project Link</div>
+  </header>
+);
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import NavBar from "./components/navbar/navbar.jsx";
 
 ReactDOM.render(
   <React.StrictMode>
+    <NavBar />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
