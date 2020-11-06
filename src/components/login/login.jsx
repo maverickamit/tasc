bcd8682adc7d30c7176861b60e91358458224316
Amit <29959138+maverickamit@users.noreply.github.com>
2020-11-06 12:43:30 +0530

    linked forgot-password page to login page forgot password button

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -114,7 +114,7 @@ function UserLogin() {
             </Button>
             <Grid container>
               <Grid item xs>
-                <Link href="#" variant="body2">
+                <Link href="/forgot-password" variant="body2">
                   Forgot password?
                 </Link>
               </Grid>
